6b096efc8741e0556385b16553d0ec8206206d8c
SAMEER HANIF <125283065+muneerattari@users.noreply.github.com>
2023-04-11 15:57:01 +0500

    updated

---
+++ assisignment1.js
@@ -0,0 +1,17 @@
+var num  = +prompt("type 1st number");
+var num2  = +prompt("type 2nd number");
+
+
+
+
+if (num > num2)
+
+{
+    console.log (num);
+}
+
+else if ()
+
+{
+    console.log (num2);
+}
